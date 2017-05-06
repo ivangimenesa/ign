6cfb3bf56545ddb72ed0bf13fbba6adae5d0a711
ivangimenesa <igntemplates@gmail.com>
2017-05-06 18:30:16 -0300

    Update Button-back-top.js

--- Button-back-top.js
+++ Button-back-top.js
@@ -21,9 +21,3 @@ $(document).ready(function() {
       return false;
          });
      });
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
