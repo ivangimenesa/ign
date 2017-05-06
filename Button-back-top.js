460e72e4577f77310c8776bbf65a41382a7fc8c8
ivangimenesa <igntemplates@gmail.com>
2017-05-06 18:31:25 -0300

    Update Button-back-top.js

--- Button-back-top.js
+++ Button-back-top.js
@@ -1,12 +1,6 @@
         // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
-function toTop(){
-$('html, body').animate({
-scrollTop: 0
-}, 1000, 'easeOutElastic');
-}
-
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("backtop").style.display = "block";
