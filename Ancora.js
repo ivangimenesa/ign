3448ed4beb779bd85e0d0f4862c82db40d768e49
ivangimenesa <igntemplates@gmail.com>
2017-05-07 02:51:48 -0300

    Update Ancora.js

--- Ancora.js
+++ Ancora.js
@@ -1,9 +1,6 @@
-$('.menu a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
 });
